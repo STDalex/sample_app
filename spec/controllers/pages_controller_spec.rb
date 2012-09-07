5ff2dbb2894d5acef00e922c5d297bcbf67b0029
= <stadnitskya@gmail.com>
2012-09-07 13:24:57 +0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
 describe PagesController do
-
+render_views
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should have the reight titlte" do
+      get 'home'
+      response.should have_selector("title", :content => "Home")
     end
   end
 
@@ -13,6 +17,21 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+    it "should have the reight titlte" do
+      get 'contact'
+      response.should have_selector("title", :content => "Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the reight titlte" do
+      get 'about'
+      response.should have_selector("title", :content => "About")
     end
   end
 
