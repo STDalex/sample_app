4d82cd2115bf4c2ab6904f02aaef44451c189f9a
= <stadnitskya@gmail.com>
2012-09-26 17:58:16 +0400

    adding email format validate for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   
-  validates :name, :presence => true
+  validates :name,  :presence => true,
+                    :length => { :maximum => 50 }
+                    
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => true,
+                    :format => { :with => email_regex }
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,5 +20,36 @@ describe User do
     User.create!(@attr)
   end
   
-  it "should require a name"
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require a email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject names that are to long" do
+    long_name = "a"*51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+  
+  it "should accept valid email address" do
+    valid_addresses = %w(user@yandex.ru THE_USER@example.bar.org first.last@example.ru)
+    valid_addresses.each do |address|
+      valid_email_address = User.new(@attr.merge(:email => address))
+      valid_email_address.should be_valid
+    end
+  end
+  
+  it "should reject invalid email address" do
+    addresses = %w(user@yandex,com user_fail.org example.user@foo.)
+    addresses.each do |address|
+      invalid_email_address = User.new(@attr.merge(:email => address))
+      invalid_email_address.should_not be_valid
+    end
+  end
+      
 end
